71a643028fc8cc54dd70b6be6b9aadc6f73e0994
Lisa Lampen <monalisa408@yahoo.com>
2020-08-19 08:29:12 -0400

    added more http commands

--- task-force-frontend/src/app/task.service.ts
+++ task-force-frontend/src/app/task.service.ts
@@ -11,7 +11,29 @@ export class TaskService {
 
   constructor(private http: HttpClient) { }
 
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(`http:18.217.160.143/todos/${task.id}`, task);
+  }
+  
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`http:18.217.160.143/todos/${id}`);
+  }
+
+  deleteAllTasks(): Observable<any> {
+    return this.http.delete('http:18.217.160.143/todos/');
+  }
+
+  getTask(id: number): Observable<Task> {
+    return this.http.get<Task>(`http:18.217.160.143/todos/${id}`);
+  }
+  
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(`http://18.222.193.153:8080/todos`);
+  }
+
+  // TODO:  routine for patch
+  
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>('http:18.217.160.143/todos/', task);
   }
 }
