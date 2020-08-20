608d860c1ca94c8cf8eb6a42319a40ab204e1c74
Justin_Mata <justinmata2@yahoo.com>
2020-08-19 22:30:21 -0700

    Mock up of Task Details view using cards. Also added a route for task Details

--- task-force-frontend/src/app/app-routing.module.ts
+++ task-force-frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { TaskPageComponent } from './task/task-page/task-page.component';
+import { TaskDetailComponent } from './task/task-detail/task-detail.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'tasks',
     component: TaskPageComponent
+  },
+  {
+    path: 'taskDetails',
+    component: TaskDetailComponent
   },
   {
     path: '**',
